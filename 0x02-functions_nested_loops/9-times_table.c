42216897028e00211ce924b36d80233e80fb05b8
manga4th <mangabappah@gmail.com>
2022-09-22 04:08:16 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -26,14 +26,12 @@ void times_table(void)
 			{
 				_putchar(' ');				
 			}
-				_putchar((d % 10) + '0');				
-			
+				_putchar((d % 10) + '0');
 			if (cone < 9)				
 			{
 				_putchar(',');
 				_putchar(' ');					
-			}
-			
+			}	
 		}		
 		_putchar('\n');			
 	}
