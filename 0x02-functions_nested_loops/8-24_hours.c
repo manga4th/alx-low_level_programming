54b7cfc3305f1664bb3db22d28eae29ad1fb0915
manga4th <mangabappah@gmail.com>
2022-09-22 03:48:27 -0700

    jack24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,14 +3,10 @@
 /**
  * jack_bauer - function that prints every minute of the day, 24 hour clock
  * h = hour, m = minutes
- *
- *    * / 10 allows second digit to rotate
- *
- *     * for loop breaks before passing 24:00
- *
- *      * Return: 24 hour clock line by line
- *
- *       */
+ * / 10 allows second digit to rotate
+ * for loop breaks before passing 24:00
+ * Return: 24 hour clock line by line
+ */
 
 void jack_bauer(void)
 {
