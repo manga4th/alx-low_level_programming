2054cbeaa82644c4a4d00e1f8ea3dfeb1945ad2b
manga4th <mangabappah@gmail.com>
2022-09-22 08:10:23 -0700

    positive or negative test

--- 0x03-debugging/main.c
+++ 0x03-debugging/main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * main - Test function for positive or negative
  * Return: 0
@@ -11,5 +12,5 @@ int main(void)
 	i = 0;
 	positive_or_negative(i);
 
-return (0);
+	return (0);
 }
