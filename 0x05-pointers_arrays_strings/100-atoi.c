3c49c76da04abf5768ad6cb1e45c777298f43274
manga4th <mangabappah@gmail.com>
2022-09-25 11:49:52 -0700

    new

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,8 +5,22 @@
  * @s: input
  * Return: Always 0 (Success)
  */
-int _atoi()
+int _atoi(char *s)
 {
+	int sign = 1;
+	unsigned int num = 0;
 
-	return (0);
+	do {
+		if (*s == '-')
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+
+	} while (*s++);
+
+	return (num * sign);
 }
